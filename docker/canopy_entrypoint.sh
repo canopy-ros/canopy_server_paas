4ef19ff2a7fcf5c7dcc38401f59a2fec222f4a7c
Raphael Chang <raphaelchang1227@gmail.com>
2016-03-10 01:28:06 +0000

    More renaming

--- docker/canopy_entrypoint.sh
+++ docker/canopy_entrypoint.sh
@@ -4,5 +4,5 @@ set -e
 # setup ros environment
 source "/opt/ros/$ROS_DISTRO/setup.bash"
 source "/root/catkin_ws/devel/setup.bash"
-roslaunch roscloud_client roscloud_container.launch host:=$HOST port:=$PORT key:=$KEY container_name:=$NAME &
+roslaunch canopy_client canopy_leaflet.launch host:=$HOST port:=$PORT key:=$KEY container_name:=$NAME &
 exec "$@"
