b384b88bd98b698e01910b7a5001e9c70d3af7bf
Raphael Chang <raphaelchang@mit.edu>
2016-12-07 02:18:43 -0500

    Update canopy_entrypoint.sh

--- docker/canopy_entrypoint.sh
+++ docker/canopy_entrypoint.sh
@@ -7,8 +7,8 @@ source "/root/catkin_ws/devel/setup.bash"
 chmod +x /package_loader.sh
 cd ~/catkin_ws/src/canopy_client
 git pull
-cd ~/catkin_ws
-catkin_make
+# cd ~/catkin_ws
+# catkin_make
 cd /
 sh ./package_loader.sh
 # roslaunch canopy_client canopy_leaflet.launch host:=$HOST port:=$PORT key:=$KEY container_name:=$NAME &
